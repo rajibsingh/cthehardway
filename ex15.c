26f34cf9f7d51fb56318abbfaf784f0dc3e231a8
Raj Singh <rajibsingh@gmail.com>
2015-11-21 22:47:03 -0500

    ex15 finished

--- ex15.c
+++ ex15.c
@@ -15,15 +15,28 @@ int main(int argc, char *argv[])
 		printf("%s has %d years alive.\n", names[i], ages[i]);
 	}
 	printf("1--------------\n");
-	return 0;
 
 	//setup the pointer
 	int *cur_age = ages;
-	char **cur_names = names;
+	char **cur_name = names;
 
 	//second way using pointers
 	for (i = 0; i < count; i++) {
-		printf("%s has %d years alive.\n", *(cur_age+1), *(cur_names + 1));
+		printf("%s has %d years alive.\n", *(cur_name + i), *(cur_age + i));
 	}
 	printf("--------------\n");
+
+	//third way, pointers are just arrays
+	for (i = 0; i < count; i++) {
+		printf("%s has %d years alive.\n", cur_name[i], cur_age[i]);
+	}
+	printf("--------------\n");
+
+	// fourth way with pointers in a stupid complex way
+	for(cur_name = names, cur_age = ages; (cur_age - ages) < count; cur_name++, cur_age++)
+	{
+		printf("%s has %d years alive.\n", *cur_name, *cur_age);
+	}
+
+	return 0;
 }
